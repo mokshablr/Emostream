056947e6dc95fb1d660ece9ea9ca00b4e84139e1
Arjun Maniyani <arjunrmaniyani@gmail.com>
2024-11-20 00:49:24 +0530

    Updated nginx-setup

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -40,31 +40,7 @@ http {
         listen 80;
 
         location / {
-            proxy_pass http://flask_servers;
-            proxy_set_header Host \\\$host;
-            proxy_set_header X-Real-IP \\\$remote_addr;
-            proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \\\$scheme;
-        }
-
-        location /emoji {
-            proxy_pass http://flask_servers/emoji;
-            proxy_set_header Host \\\$host;
-            proxy_set_header X-Real-IP \\\$remote_addr;
-            proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \\\$scheme;
-        }
-
-        location /send_emoji {
-            proxy_pass http://flask_servers/send_emoji;
-            proxy_set_header Host \\\$host;
-            proxy_set_header X-Real-IP \\\$remote_addr;
-            proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto \\\$scheme;
-        }
-
-        location /processed_emojis {
-            proxy_pass http://flask_servers/processed_emojis;
+            proxy_pass http://flask_servers\\\$request_uri;
             proxy_set_header Host \\\$host;
             proxy_set_header X-Real-IP \\\$remote_addr;
             proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
